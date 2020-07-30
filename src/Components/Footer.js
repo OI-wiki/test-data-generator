a96df11e69df340f8c3dab705cac41aabcd4d7e6
lynklody <liuai_1995@live.cn>
2020-07-31 02:02:11 +0800

    temp*: temporarily disable theme to make travis happy

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -2,17 +2,18 @@ import React from 'react'
 import { Container, Typography, CardContent } from '@material-ui/core'
 import { makeStyles } from '@material-ui/core/styles'
 
-const useStyles = makeStyles( (theme) => ({
-    content: {
-        color: theme.palette.footer.text
-    }
-}));
+// const useStyles = makeStyles( (theme) => ({
+//     content: {
+//         color: theme.palette.footer.text
+//     }
+// }));
 
 const Footer = () => {
-    const classes = useStyles()
+    // const classes = useStyles()
     return(
         <Container>
-            <Typography className={classes.content} component={"span"}>
+            <Typography component={"span"}>
+            {/* <Typography className={classes.content} component={"span"}> */}
                 <CardContent>
                     footer
                 </CardContent>
