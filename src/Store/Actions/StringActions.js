61ee5c4c523da5d1964d0fd34846d9d8e9fdd2e7
lynklody <liuai_1995@live.cn>
2020-08-10 21:08:50 +0800

    +: string gen action & changes in string input UI

--- src/Store/Actions/StringActions.js
+++ src/Store/Actions/StringActions.js
@@ -7,9 +7,47 @@ export const startStringGen = (state) => {
     }
     let result = ''
     let arr = chars.split('')
-    console.log(arr)
-    for (let i = 0; i < numCases; ++i) {
-        result += arr[Math.floor(Math.random() * arr.length)]
+    if (allowDuplicate) {
+        console.log('ALLOW dup')
+        if (delimiter === '') {
+            console.log('WITHOUT delimiter')
+            for (let i = 0; i < numCases; ++i) {
+                result += arr[Math.floor(Math.random() * arr.length)]
+            }
+        }
+        else {
+            console.log('WITH delimiter')
+            for (let i = 0; i < numCases; ++i) {
+                for (let j = 0; j < numChars; ++j) {
+                    result += arr[Math.floor(Math.random() * arr.length)]
+                }
+                if (i !== numCases-1)
+                    result += delimiter
+            }
+        }
+
+    }
+    else {
+        console.log('dup NOT ALLOWED')
+        let count = Math.min(arr.length, numCases)
+        if (delimiter === '') {
+            console.log('WITHOUT delimiter')
+            while (count > 0 && arr.length > 0) {
+                let idx = Math.floor(Math.random() * count--)
+                result += arr[idx]
+                arr.splice(idx, 1)
+            }
+        }
+        else {
+            console.log('WITH delimiter')
+            while (count > 0 && arr.length > 0) {
+                let idx = Math.floor(Math.random() * count--)
+                result += arr[idx]
+                arr.splice(idx, 1)
+                if (count !== 0)
+                    result += delimiter
+            }
+        }
     }
     console.log(result)
 }
