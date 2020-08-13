dd5d9e6b16a8959aa23813876f20f30b9694169a
lynklody <liuai_1995@live.cn>
2020-08-13 23:15:56 +0800

    +: output area functionality

--- src/Components/OutputArea.js
+++ src/Components/OutputArea.js
@@ -1,6 +1,7 @@
-import React from 'react'
-import { Container, Card, CardContent, Grid } from '@material-ui/core'
+import React, {useEffect, useState} from 'react'
+import { Container, Card, CardContent, Grid, TextField } from '@material-ui/core'
 import { makeStyles } from '@material-ui/core/styles'
+import { useSelector } from 'react-redux'
 
 const useStyles = makeStyles({
   outputBox: {
@@ -14,18 +15,42 @@ const useStyles = makeStyles({
   }
 })
 
-const renderOutputArea = () => {
-  return (
-      <Container>
-          <CardContent>
-              output
-          </CardContent>
-      </Container>
-  )
+const renderOutputArea = (output) => {
+    console.log("output=",output)
+    if (output === '') {
+        return (
+            <Container>
+                <CardContent>
+                    测试用例输出：
+                </CardContent>
+                <CardContent>
+                    请选择测试用例的数据类型
+                </CardContent>
+            </Container>
+        )
+    }
+    else {
+        return (
+            <Container>
+                <CardContent>
+                    测试用例输出：
+                </CardContent>
+                <TextField
+                    label=""
+                    multiline
+                    rows={8}
+                    defaultValue={output}
+                    variant="filled"
+                />
+            </Container>
+        )
+    }
 }
 
 const OutputArea = () => {
-    const classes = useStyles();
+    const classes = useStyles()
+    const output = useSelector(state => state.output)
+    
     return(
         <Container fixed
             className={classes.outputBox}
@@ -39,7 +64,7 @@ const OutputArea = () => {
                     className={classes.card}
                 >
 
-                    { renderOutputArea() }
+                    { renderOutputArea(output) }
 
                 </Card>
             </Grid>
