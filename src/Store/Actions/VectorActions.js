aafd0f6b17d32d34633333753c433b145c6126db
lynklody <liuai_1995@live.cn>
2020-09-25 14:32:24 -0700

    *: one more file change for vector float range input

--- src/Store/Actions/VectorActions.js
+++ src/Store/Actions/VectorActions.js
@@ -3,7 +3,7 @@ import { SET_OUTPUT } from './ActionTypes'
 export const startVectorGen = (state, dispatch) => {
     console.log("STRING GEN ACTIONS")
     const { chars, numChars, numCases, allowDuplicate, output } = state.rootReducer
-    const { typeChecked } = state.VectorReducer
+    const { string, int, float, vector } = state.VectorReducer
     if (output !== '') {
         console.log('ERROR: output not empty, submit clicked more than once')
     }
