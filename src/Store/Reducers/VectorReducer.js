8ccb29fb6a373b7d9ae86dfe643831ca24aa116f
lynklody <liuai_1995@live.cn>
2020-09-25 22:20:07 -0700

    +: hmm..one more file for the same feature

--- src/Store/Reducers/VectorReducer.js
+++ src/Store/Reducers/VectorReducer.js
@@ -1,16 +1,25 @@
-import { UPDATE_VECTOR_ELEM_TYPE_str, UPDATE_VECTOR_ELEM_TYPE_int, UPDATE_VECTOR_ELEM_TYPE_float, ALTER_ALL } from "../Actions/ActionTypes";
+import { UPDATE_VECTOR_ELEM_TYPE_str, UPDATE_VECTOR_ELEM_TYPE_int, 
+    UPDATE_VECTOR_ELEM_TYPE_float, ALTER_ALL, UPDATE_NUM_ELEMENTS,
+    UPDATE_FLOAT_RANGE } from "../Actions/ActionTypes";
 
 const initState = {
+    numElem: 0,
     typeChecked: {
         string: false,
         int: false,
         float: false,
         vector: false, // TODO
     },
+    floatRange: ''
 }
 
 const VectorReducer = (state=initState, action) => {
     switch(action.type) {
+        case UPDATE_NUM_ELEMENTS:
+            return {
+                ...state,
+                numElem: action.payload
+            }
         case UPDATE_VECTOR_ELEM_TYPE_str:
             return {
                 ...state,
@@ -34,6 +43,11 @@ const VectorReducer = (state=initState, action) => {
                     ...state.typeChecked,
                     float: action.payload
                 }
+            }
+        case UPDATE_FLOAT_RANGE:
+            return {
+                ...state,
+                floatRange: action.payload
             }
         case ALTER_ALL:
             return {
